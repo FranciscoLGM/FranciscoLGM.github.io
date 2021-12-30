8fc8ee05f59922773325b4b34e819046a22038b5
Francisco Javier López García Mayorga <javier6282@gmail.com>
2021-12-30 20:37:01 -0300

    templates for movies

--- js/api.js
+++ js/api.js
@@ -1,18 +1,31 @@
 // template para pagina de peliculas
+const items = document.getElementById('item')
+const templateMovie = document.getElementById('template-movie').content // template para peliculas 
+const fragment = document.createDocumentFragment()
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    peliculas()
-  })
-  
-  const peliculas = async () => {
-    try {
-      const response = await fetch('api.json')
-      const data = await response.json()
-      console.log(data)
-    } catch (error) {
-      console.log(error)
-    }
+  fetchData()
+})
+
+const fetchData = async () => {
+  try {
+    const response = await fetch('../js/api.json')
+    const data = await response.json()
+    //console.log(data)
+    renderMovie(data)
+  } catch (error) {
+    console.log(error)
   }
-  
-  
-  
+}
+
+const renderMovie = data => {
+  data.forEach(movie => {
+    templateMovie.querySelector('p').textContent = movie.title
+    templateMovie.querySelector('img').src = movie.url
+
+    const clone = templateMovie.cloneNode(true)
+    fragment.appendChild(clone)
+  });
+  items.appendChild(fragment)
+}
